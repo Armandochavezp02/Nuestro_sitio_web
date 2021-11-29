5fd2a2fd6b69cb18872ba5500157fee0ac8a991f
Said Sachez <91161146+SaidSanchez@users.noreply.github.com>
2021-11-28 19:08:32 -0700

    Se agrega la primer parte del script del servidor

---
+++ app.js
@@ -0,0 +1,23 @@
+const http = require('http'); //libreria de http
+const fs = require('fs'); //libreria de file system
+
+
+console.log("Servidor ejecutandose...");
+
+
+http.createServer((request, response) => {
+  console.log(request.url);
+  const file = request.url == '/' ? "./index.html" : `.${request.url}`;
+
+  fs.readFile(file, (error, data) => {
+    if (error) {
+      response.writeHead(404, {
+        "Content-Type": "text/plain"
+      });
+      response.write("No found data");
+      response.end();
+    }
+  });
+
+
+}).listen(8888);
