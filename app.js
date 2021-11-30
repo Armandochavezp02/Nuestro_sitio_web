70e7d282b5ae418935ae6f88e9324d42a064fd63
Said Sachez <91161146+SaidSanchez@users.noreply.github.com>
2021-11-30 02:46:30 -0700

    Servidor Funcional

--- app.js
+++ app.js
@@ -1,23 +1,42 @@
 const http = require('http'); //libreria de http
 const fs = require('fs'); //libreria de file system
-
+const path=require('path');
+  
 
 console.log("Servidor ejecutandose...");
+http.createServer((req, res) => {
 
+  console.log(`${req.method} solicita ${req.url}`);
 
-http.createServer((request, response) => {
-  console.log(request.url);
-  const file = request.url == '/' ? "./WWW/index.html" : `./${request.url}`;
-
-  fs.readFile(file, (error, data) => {
-    if (error) {
-      response.writeHead(404, {
-        "Content-Type": "text/plain"
+  if(req.url =='/'){
+    fs.readFile('./WWW/index.html', 'UTF-8',(err,html)=>{
+      res.writeHead(200,{'Content-Type':'text/html'});
+      res.end(html);
       });
-      response.write("No found data");
-      response.end();
-    }
-  });
+      }else if(req.url.match(/.css$/)){
+          const reqPath=path.join(__dirname,'WWW',req.url);
+          const fileStream=fs.createReadStream(reqPath,'UTF-8');
+          res.writeHead(200 ,{'Content-Type':'text/css'});
+          fileStream.pipe(res);
+          
+        }else if(req.url.match(/.png$/)){
+          const reqPath=path.join(__dirname,'WWW',req.url);
+          const fileStream=fs.createReadStream(reqPath);
+          res.writeHead(200 ,{'Content-Type':'text/png'});
+          fileStream.pipe(res);
+
+
+        }else if(req.url.match(/.jpg$/)){
+          const reqPath=path.join(__dirname,'WWW',req.url);
+          const fileStream=fs.createReadStream(reqPath);
+          res.writeHead(200 ,{'Content-Type':'text/jpg'});
+          fileStream.pipe(res);
+        }else{
+        res.writeHead(404,{'Content-Type':'text/plain'});
+        res.end('Server Error');
+
+      }
 
 
 }).listen(8888);
+
